05a8c3f6b6d37c11d99a002ab29a52e78cf4d8a0
Dipesh mandal <dipesh314159@gmail.com>
2025-09-15 11:29:52 +0545

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+data ={'Name':["dipesh","nitesh","ram"],
+       "Age":[23,30,35],
+       "city":["new work","los angeles","chicago"]
+    
+}
+df = pd.DataFrame(data)
+print(df)
+
+data_dir ="data"
+os.makedirs(data_dir,exist_ok=True)
+
+file_path = os.path.join(data_dir,"sample_data.csv")
+
+df.to_csv(file_path,index=False)
+print(f"csv file saved to {file_path}")
+
